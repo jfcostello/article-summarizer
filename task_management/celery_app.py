597962f738b26af33a9e22289aedcc7d757ec617
Jamie Costello <jamie@jfcostello.com>
2024-06-06 11:25:29 -0400

    It seems to work!? Will require testing, but so far so good modified:   task_management/celery_app.py

--- task_management/celery_app.py
+++ task_management/celery_app.py
@@ -59,9 +59,8 @@ def fetch_urls():
             total_new_urls = 0
 
         if total_new_urls > 0:
+            print("DEBUG: Adding 'execute_additional_tasks' to the task queue")
             task_queue.append('execute_additional_tasks')
-            print("DEBUG: Added 'execute_additional_tasks' to the task queue") 
-        # else:  # You might want to log here if no URLs were found
 
         return total_new_urls
 
@@ -94,16 +93,16 @@ def tagging(self, *args, **kwargs):
 
 @app.task(name='task_management.celery_app.process_task_queue')
 def process_task_queue():
-    print("DEBUG: process_task_queue function called.") 
+    print("DEBUG: process_task_queue function called.")
     if task_queue:
-        print(f"DEBUG: Task queue contains: {task_queue}") 
+        print(f"DEBUG: Task queue contains: {task_queue}")
         task_name = task_queue.pop(0)
         if task_name == 'execute_additional_tasks':
-            execute_additional_tasks.delay()
+            execute_additional_tasks.delay() # Launch the chain
 
-@app.task(name='task_management.celery_app.trigger_queue_processing') # New task for manual trigger
-def trigger_queue_processing():
-    process_task_queue.delay()  # Call the queue processing task
+# @app.task(name='task_management.celery_app.trigger_queue_processing') # New task for manual trigger, this is for debugging
+# def trigger_queue_processing():
+    # process_task_queue.delay()  # Call the queue processing task
 
 @app.task(name='task_management.celery_app.execute_additional_tasks')
 def execute_additional_tasks():
